ecf955422b79a21efecdb4697b9ed1350d1ddbbc
gerardo jerez <gerardo.jerez@hotmail.com>
2019-11-05 12:44:09 -0700

    camara

--- helloWorld.js
+++ helloWorld.js
@@ -68,9 +68,35 @@ const initBuffers = gl =>{
     };
 }
 
+const drawScene = (gl,programInfo, buffers) =>{
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clearDepth(1);
+    gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL);
+
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    //Camara//
+    const fieldOfView = 45 * Math.PI / 180;
+    const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+    const zNear = 0.1;
+    const zFar = 100;
+    const projectionMatrix = mat4.create();
+
+    mat4.perspective(
+        projectionMatrix,
+        fieldOfView,
+        aspect,
+        zNear,
+        zFar
+    );
+
+
+}
 
 const main = ()=>{
-    const gl = canvas.getContext('webgl');
+    const gl = canvas.getContext('webgl2');
 
     if (!gl) {
         alert("Unable to initialize WebGL. Your browser or machine may not support it.");
@@ -92,9 +118,10 @@ const main = ()=>{
     
 
     const buffers = initBuffers(gl);
+
+    drawScene(gl, programInfo, buffers);
     
-    gl.clearColor(0, 0, 0, 1);
-    gl.clear(gl.COLOR_BUFFER_BIT);
+    
 }
 
 window.onload = main;
