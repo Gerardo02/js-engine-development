153f6d783fda7bf7cfd5580c44639893d7f1ccd2
gerardo jerez <gerardo.jerez@hotmail.com>
2019-11-06 09:47:52 -0700

    colores revueltos

--- helloWorld.js
+++ helloWorld.js
@@ -3,19 +3,26 @@ const canvas = document.getElementById('glcanvas');
 // Vertex shader program
 const vsSource = `
 attribute vec4 aVertexPosition;
+attribute vec4 aVertexColor;
 
 uniform mat4 uModelViewMatrix;
 uniform mat4 uProjectionMatrix;
 
+varying lowp vec4 vColor;
+
 void main() {
-  gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
-}
-`;
+    gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+    vColor = aVertexColor;
+}`;
 
 const fsSource = `
+
+varying lowp vec4 vColor;
+
     void main() {
-      gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+        gl_FragColor = vColor;
 }`;
+
 const loadShader = (gl, type, source) =>{
     const shader = gl.createShader(type);
     gl.shaderSource(shader, source);
@@ -55,7 +62,8 @@ const initBuffers = gl =>{
          1.0,  1.0,
         -1.0, -1.0,
          1.0, -1.0,
-      ];
+    ];
+    
 
     gl.bufferData(
         gl.ARRAY_BUFFER,
@@ -63,8 +71,23 @@ const initBuffers = gl =>{
         gl.STATIC_DRAW
     );
 
+    const colors = [
+        1, 1, 1, 1, //white
+        1, 0, 0, 1, //reg
+        0, 1, 0, 1, //green
+        0, 0, 1, 1 //blue
+    ];
+    const colorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    gl.bufferData(
+        gl.ARRAY_BUFFER, 
+        new Float32Array(colors), 
+        gl.STATIC_DRAW
+    );
+
     return {
-    position: positionBuffer,
+        position : positionBuffer,
+        color : colorBuffer,
     };
 }
 
@@ -121,6 +144,26 @@ const drawScene = (gl,programInfo, buffers) =>{
         );   
     }
 
+    /** color  **/
+{
+    const numComponents = 4;
+    const type = gl.FLOAT;
+    const normalize = false;
+    const stride = 0;
+    const offset = 0;
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffers.color);
+    gl.vertexAttribPointer(
+        programInfo.attribLocations.vertexColor,
+        numComponents,
+        type,
+        normalize,
+        stride,
+        offset
+    );
+    gl.enableVertexAttribArray(
+        programInfo.attribLocations.vertexColor
+    );
+}
     gl.useProgram(programInfo.program);
     gl.uniformMatrix4fv(
         programInfo.uniformLocations.projectionMatrix,
@@ -153,11 +196,12 @@ const main = ()=>{
     const programInfo = {
         program: shaderProgram,
         attribLocations: {
-          vertexPosition: gl.getAttribLocation(shaderProgram, 'aVertexPosition'),
+            vertexPosition: gl.getAttribLocation(shaderProgram, 'aVertexPosition'),
+            vertexColor: gl.getAttribLocation(shaderProgram, 'aVertexColor'),
         },
         uniformLocations: {
-          projectionMatrix: gl.getUniformLocation(shaderProgram, 'uProjectionMatrix'),
-          modelViewMatrix: gl.getUniformLocation(shaderProgram, 'uModelViewMatrix'),
+            projectionMatrix: gl.getUniformLocation(shaderProgram, 'uProjectionMatrix'),
+            modelViewMatrix: gl.getUniformLocation(shaderProgram, 'uModelViewMatrix'),
         },
     };
     
